d29c89c317fe123273a826dfcd8e238e2ae58426
Guiba2 <Frhendgaud@hotmail.com>
2024-11-30 18:09:09 -0300

    Atualização
    acresentei mais uma parte tipo um feedback

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ let clickCa = document.getElementById("Camisa");
 let fora = document.getElementById("Fora");
 let clickNovi = document.getElementById("Novidade");
 let Desati = document.getElementById("Obj");
+let Conta = document.getElementById("Conta");
+let contaApa  = document.getElementById("VerConta");
 let click = false;
 let Camisa = false;
 let Nov = false;
 let desativar = false;
-
-
+let ContaClick = false;
+contaApa.addEventListener("mouseover", function() {
+    contaApa.addEventListener("mouseleave", function() {
+        ContaClick = false;
+        clicouConta();
+    })
+})
+ 
+Conta.addEventListener("click", function() {
+    ContaClick = true;
+    clicouConta();
+})
 
 Desati.addEventListener("mouseover", function() {
     desativar = true;
@@ -56,6 +68,13 @@ function VerNovidades() {
         Novidades.display = "none";
     } else {
 
+    }
+}
+function clicouConta(mouse) {
+    if (ContaClick) {
+        contaApa.style.opacity = "1";
+    } else {
+        contaApa.style.opacity = "0";
     }
 }
 VerNovidades();
